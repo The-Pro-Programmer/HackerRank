0f31853e445379fa0620d6469cf59c96b11d8b1b
Mansi Wakharkar <mansiswakharkar@gmail.com>
2019-06-30 22:40:34 +0530

    Height of binary of tree

---
+++ src/datastructures/trees/HeightOfBinaryTree.java
@@ -0,0 +1,43 @@
+package datastructures.trees;
+
+import java.util.Scanner;
+
+public class HeightOfBinaryTree {
+
+	public static int height(Node root) {
+      	if(root==null){
+            return -1;
+        }else{
+            return Math.max(height(root.left), height(root.right))+1;
+        }
+    }
+
+	public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        int height = height(root);
+        System.out.println(height);
+    }	
+}
